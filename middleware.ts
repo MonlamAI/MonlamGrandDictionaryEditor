01292434d3b85f49bb5e17581131cd82316b42a9
TenzDelek <tibetdelek@gmail.com>
2025-01-17 16:37:37 +0530

    dynamic route protection and codebase cleanup

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,5 @@ import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
 export default withMiddlewareAuthRequired();
 
 export const config = {
-  matcher: ["/book", "/person", "/publisher", "/word"],
+  matcher: ["/book", "/person", "/publisher", "/word/:path*"],
 };
